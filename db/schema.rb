b93b898f11baf3f9bd7c8d50da667a401e4d3862
EC2 Default User <ec2-user@ip-172-31-6-10.ap-northeast-2.compute.internal>
2016-05-30 11:23:05 +0000

    test1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516122351) do
+ActiveRecord::Schema.define(version: 20160529093647) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "string"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "writepages", force: :cascade do |t|
     t.string   "title"
